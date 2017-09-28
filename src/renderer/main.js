bf98f83f17db5aabc78ceed8a7d178d1d0cbda71
idzn <idzn@yandex.ru>
2017-09-28 16:45:42 +0300

    add bootstrap

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import axios from 'axios'
 
 import App from './App'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
