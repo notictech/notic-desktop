88e3b091f1fcc04191313705eb6064d25926d1f3
Sergey.Tolokonnikov <sergey.tolokonnikov@stackpath.com>
2018-03-18 03:16:34 +0300

    upd

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -89,8 +89,8 @@
                         <b-button-group size="sm" class="search-filters">
                             <b-button :variant="(searchFilter == 'notes') ? 'warning' : 'secondary'" title="All (Ctrl+1)" @click="setSearchFilter('notes')"><icon name="asterisk"></icon></b-button>
                             <b-button :variant="(searchFilter == 'secrets') ? 'warning' : 'secondary'" title="Secrets (Ctrl+2)" @click="setSearchFilter('secrets')"><icon name="key"></icon></b-button>
-                            <b-button :variant="(searchFilter == 'star') ? 'warning' : 'secondary'" title="Favorites (Ctrl+3)" @click="setSearchFilter('star')"><icon name="star-o"></icon></b-button>
-                            <b-button :variant="(searchFilter == 'reminder') ? 'warning' : 'secondary'" title="Reminders (Ctrl+4)" @click="setSearchFilter('reminder')"><icon name="bell-o"></icon></b-button>
+                            <b-button :variant="(searchFilter == 'star') ? 'warning' : 'secondary'" title="Favorites (Ctrl+3)" @click="setSearchFilter('star')"><icon name="star"></icon></b-button>
+                            <b-button :variant="(searchFilter == 'reminder') ? 'warning' : 'secondary'" title="Reminders (Ctrl+4)" @click="setSearchFilter('reminder')"><icon name="bell"></icon></b-button>
                         </b-button-group>
                         <b-button-group size="sm">
                             <b-btn style="margin-left: 4px;" :variant="(searchFilter == 'deleted') ? 'warning' : 'secondary'" title="Trash (Ctrl+5)" @click="setSearchFilter('deleted')"><icon name="trash"></icon></b-btn>
